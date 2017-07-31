dc762a9d9586bd8c9aec5752b0f7160e45933143
mingpeach <mjb940806@naver.com>
2017-08-01 02:42:17 +0900

    Use argc and argv

--- arp.c
+++ arp.c
@@ -8,34 +8,39 @@
 #include <unistd.h> // close
 #include <arpa/inet.h>
 
-int main()
+int main(int argc, char *argv[])
 {
 	int fd;
 	struct ifreq ifr;
 	unsigned char *mac;
 	
-	char iface[] = "ens33";
+	char *dev, *sender_ip, *target_ip;
+
+	if (argc != 4) {
+		printf("input needed: 1. dev 2. sender_ip 3. target_ip \n");
+		exit(1);
+	}
+
+	dev = argv[1];
+	sender_ip = argv[2];
+	target_ip = argv[3];
 
 	fd = socket(AF_INET, SOCK_DGRAM, 0);
 
 	//Type of address to retrieve - IPv4 IP address
 	ifr.ifr_addr.sa_family = AF_INET;
-
 	//Copy the interface name in the ifreq structure
-	strncpy(ifr.ifr_name , iface , IFNAMSIZ-1);
-
+	strncpy(ifr.ifr_name , dev , IFNAMSIZ-1);
 	ioctl(fd, SIOCGIFADDR, &ifr);
-
 	close(fd);
 
 	mac = (unsigned char *)ifr.ifr_hwaddr.sa_data;
-
 
 	//display mac address
 	printf("Mac : %.2x:%.2x:%.2x:%.2x:%.2x:%.2x\n" , mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
  
 	//display result
-	printf("%s - %s\n" , iface , inet_ntoa(( (struct sockaddr_in *)&ifr.ifr_addr )->sin_addr) );
+	printf("%s - %s\n" , dev , inet_ntoa(( (struct sockaddr_in *)&ifr.ifr_addr )->sin_addr) );
 
 	return 0;
 }
