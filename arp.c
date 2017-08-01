eb608863fd120fd0701c1f1af91e6010b038a3ec
mingpeach <mjb940806@naver.com>
2017-08-01 20:20:16 +0900

    Fix the attacker's mac address

--- arp.c
+++ arp.c
@@ -19,27 +19,27 @@
 #include <net/if_arp.h>
 #include <net/ethernet.h>
 
-/* ARP Header, (assuming Ethernet+IPv4)            */ 
+/* ARP Header, (assuming Ethernet+IPv4)                   */ 
 #define ETHERNET 1
-#define ARP_REQUEST 1   /* ARP Request             */ 
-#define ARP_REPLY 2     /* ARP Reply               */ 
+#define ARP_REQUEST 1          /* ARP Request             */ 
+#define ARP_REPLY 2            /* ARP Reply               */ 
 typedef struct arpheader { 
-    uint16_t htype;    /* Hardware Type           */ 
-    uint16_t ptype;    /* Protocol Type           */ 
-    uint8_t hlen;        /* Hardware Address Length */ 
-    uint8_t plen;        /* Protocol Address Length */ 
-    uint16_t oper;     /* Operation Code          */ 
+    uint16_t htype;            /* Hardware Type           */ 
+    uint16_t ptype;            /* Protocol Type           */ 
+    uint8_t hlen;              /* Hardware Address Length */ 
+    uint8_t plen;      	       /* Protocol Address Length */ 
+    uint16_t oper;	       /* Operation Code          */ 
     unsigned char sha[6];      /* Sender hardware address */ 
-    uint32_t spa;      /* Sender IP address       */ 
+    uint32_t spa;              /* Sender IP address       */ 
     unsigned char tha[6];      /* Target hardware address */ 
-    uint32_t tpa;      /* Target IP address       */ 
+    uint32_t tpa;              /* Target IP address       */ 
 } __attribute__((packed)) arphdr_t; 
 
 int main(int argc, char *argv[])
 {
 	int fd;
 	struct ifreq ifr;
-	unsigned char *attacker_mac[6];
+	unsigned char attacker_mac[6];
 	uint32_t *attacker_ip;
 	char *dev, *sender_ip, *target_ip;
 	
@@ -64,18 +64,19 @@ int main(int argc, char *argv[])
 	target_ip = argv[3];
 
 	fd = socket(AF_INET, SOCK_DGRAM, 0);
+	if(fd < 0) perror("socket fail");
 
 	/* Type of address to retrieve - IPv4 IP address */
-	ifr.ifr_addr.sa_family = AF_INET;
+	//ifr.ifr_addr.sa_family = AF_INET;
 	/* Copy the interface name in the ifreq structure */
 	strncpy(ifr.ifr_name , dev , IFNAMSIZ-1);
-	ioctl(fd, SIOCGIFADDR, &ifr);
+	if(ioctl(fd, SIOCGIFHWADDR, &ifr) < 0) perror("ioctl fail");
 	close(fd);
 
-	memcpy(attacker_mac, (unsigned char *)ifr.ifr_hwaddr.sa_data, 6);
+	memcpy(attacker_mac, ifr.ifr_hwaddr.sa_data, 6);
 
 	/* Display mac address */
-	//printf("Mac : %.2x:%.2x:%.2x:%.2x:%.2x:%.2x\n" , attacker_mac[0], attacker_mac[1], attacker_mac[2], attacker_mac[3], attacker_mac[4], attacker_mac[5]);
+	//printf("Mac : %s:%s:%s:%s:%s:%s\n" , attacker_mac[0], attacker_mac[1], attacker_mac[2], attacker_mac[3], attacker_mac[4], attacker_mac[5]);
  
 	/* Display ip address */
 	attacker_ip = ((struct sockaddr_in *)&ifr.ifr_addr )->sin_addr.s_addr;
@@ -83,7 +84,7 @@ int main(int argc, char *argv[])
 	//attacker_ip = (((struct sockaddr_in *)&ifr.ifr_addr )->sin_addr.s_addr);
 
 	/* Open network device for packet capture */ 
-	if((handle = pcap_open_live(dev, BUFSIZ, 1, 1000, errbuf))==NULL) {
+	if((handle = pcap_open_live(dev, BUFSIZ, 1, 1, errbuf))==NULL) {
 		printf("Couldn't open device %s : %s\n", dev, errbuf);
 		return 2;
 	}
@@ -98,8 +99,8 @@ int main(int argc, char *argv[])
 	arpheader = (struct arpheader *)(packet+14);
 	arpheader->htype = ntohs(ETHERNET);
 	arpheader->ptype = ntohs(ETHERTYPE_IP);
-	arpheader->hlen = 6;
-	arpheader->plen = 4;
+	arpheader->hlen = sizeof(arpheader->sha); 
+	arpheader->plen = sizeof(arpheader->spa);
 	arpheader->oper = ntohs(ARP_REQUEST);
 	memcpy(arpheader->sha, attacker_mac, 6);
 	arpheader->spa = attacker_ip;
